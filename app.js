df5d36fd2b63920d795c58264b5e523d3cdd96a2
Dragos Boboluta <83366737+DragosBobo@users.noreply.github.com>
2022-03-12 13:25:10 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express =require("express");
+const app=express();
+const bodyParser=require("body-parser");
+
+let items=[];
+
+app.set("view engine","ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+app.get("/",function(req,res){
+app.post("/",function(req,res){
+   console.log("hello");
+});
+
+let today = new Date();
+
+let options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+};
+let day= today.toLocaleDateString("en-US",options);
+  res.render("list",{kindOfDay: day, newListItems:items}); 
+});
+app.post("/",function(req,res){
+    let item =req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.listen(3000,function(){
+    console.log("Server started on port 3000");})
